c727e20e4ceb13e819d4b7fe2ad98a8db3a5d09d
Favour Ukonu <favouroukonu@gmail.com>
2024-07-03 20:03:27 +0100

    Feat: Chore: Fitness Clinic

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "bg-primary": "#010101",
+        "text-primary": "#ABABAB",
+        "text-secondary": "#FFFFFF",
+        "green-primary": "#25AB75",
+      },
+      fontFamily: {
+        poppins: "Poppins, sans-serif",
       },
     },
   },
